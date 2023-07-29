8aaf4e10df2f88a05a557e24531a82ffc096eaf9
Gaspare Tortora <g.tortora2@gmail.com>
2023-07-28 21:19:15 -0400

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+import './App.css'
+
+function App() {
+
+  useEffect(() => {
+    const apiKey = import.meta.env.VITE_API_KEY;
+  }, [])
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
