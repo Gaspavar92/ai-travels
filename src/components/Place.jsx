5042d57eb77b2c46e398aeae84f471e658947e71
Gaspare Tortora <g.tortora2@gmail.com>
2023-08-04 10:38:55 -0400

    Created place component that handle location search

--- src/components/Place.jsx
+++ src/components/Place.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
+import Loading from './Loading'
+import "./styles/Place.css"
 
-const Place = ({user_place}) => {
+const Place = ({user_place, selectFunction}) => {
 
     const [places, setPlaces] = useState([]);
+    const [loading, setLoading] = useState(false);
+
+    const handleClick = (name, address, image, rating, types) => {
+        selectFunction(name, address, image, rating, types);
+    }
 
     const apiKey = import.meta.env.VITE_MAPS_KEY;
 
@@ -16,35 +23,50 @@ useEffect(() => {
         })
 
         url.search = params;
+
+        try {
+            setLoading(true);
+            const response = await fetch(url);
+            const data = await response.json();
+            const places = data.results;
+            console.log(places)
     
-        const response = await fetch(url);
-        const data = await response.json();
-        const places = data.results;
+    
+            // START Retrieving all places and pictures
+    
+                const placesWithPics = [];
+    
+                for (let place of places) {
 
-
-        // START Retrieving all places and pictures
-
-            const placesWithPics = [];
-
-            for (let place of places) {
-                const photoRef = place.photos[0].photo_reference;
-                const photoURL = new URL("https://proxy.junocollege.com/https://maps.googleapis.com/maps/api/place/photo");
-                const photoParams = new URLSearchParams({
-                    maxwidth: 400,
-                    photo_reference: photoRef,
-                    key: apiKey
-                })
-        
-                photoURL.search = photoParams;
-        
-                const photoResponse = await fetch(photoURL);
-                const photoLink = await photoResponse.blob();
-        
-                const picURL = URL.createObjectURL(photoLink);
-                placesWithPics.push({...place, url: picURL})
-            }
-
-            setPlaces(placesWithPics);
+                    if (place.photos) {
+                        const photoRef = place.photos[0].photo_reference;
+                        const photoURL = new URL("https://proxy.junocollege.com/https://maps.googleapis.com/maps/api/place/photo");
+                        const photoParams = new URLSearchParams({
+                            maxwidth: 400,
+                            photo_reference: photoRef,
+                            key: apiKey
+                        })
+    
+                            photoURL.search = photoParams;
+                    
+                            const photoResponse = await fetch(photoURL);
+                            const photoLink = await photoResponse.blob();
+                    
+                            const picURL = URL.createObjectURL(photoLink);
+                            placesWithPics.push({...place, url: picURL})
+                    } else {
+                        const picURL = "../src/assets/no-image.png"
+                        placesWithPics.push({...place, url: picURL})
+                    }
+                }
+    
+                setPlaces(placesWithPics);
+        } catch {
+            console.log('ERROR!')
+        } finally {
+            setLoading(false);
+        }
+    
 
         // END Setting up all places and pics
     }
@@ -54,12 +76,16 @@ useEffect(() => {
         }, [user_place])
 
         return (
+            loading ?
+            <Loading /> :
             <ul className="all-places">
                 {places.map((place) => {
                     return (
-                            <li key={place.place_id}>
+                            <li key={place.place_id} className="list-place">
                                 <h2>{place.name}</h2>
-                                <img src={place.url}></img>
+                                {place.formatted_address && <h3>{place.formatted_address}</h3>}
+                                <img src={place.url} className="location"></img>
+                                <button className="select-button" onClick={() => {handleClick(place.name, place.formatted_address, place.url, place.rating, place.types)}}>Select</button>
                             </li>
                     )
                 })}
