080040320fe949123fa181c1536a4c14f180d6e7
Gaspare Tortora <g.tortora2@gmail.com>
2023-08-04 10:34:57 -0400

    Created Loading component to handle loading state

---
+++ src/components/Loading.jsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+    return (
+        <div className="loading">Loading
+            <span className="loading-dot"></span>
+        </div> 
+    )
+}
+
+export default Loading;
