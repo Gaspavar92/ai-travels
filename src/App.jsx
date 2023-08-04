5549be11ae19c11a3ff885abb7b074ad184b8c4f
Gaspare Tortora <g.tortora2@gmail.com>
2023-08-04 10:40:28 -0400

    Created web app logic and component rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 
-import Sidebar from './components/Sidebar';
 import Place from './components/Place';
+import Itinerary from './components/Itinerary'
 
 import './App.css'
 
@@ -10,6 +10,12 @@ function App() {
   const [search, setSearch] = useState('');
   const [place, setPlace] = useState('');
 
+// Conditionally rendering the components
+  const [showPlace, setShowPlace] = useState(false);
+  const [showItinerary, setShowItinerary] = useState(false);
+
+  const [selectedPlace, setSelectedPlace] = useState([]);
+
   const handleChange = (e) => {
     e.preventDefault();
     setSearch(e.target.value)
@@ -17,19 +23,46 @@ function App() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setShowItinerary(false);
+    setShowPlace(true);
     setPlace(search);
+    setSearch('');
+  }
+
+  const handleSelect = (name, address, image, rating, types) => {
+    const placeInfo = {name, address, image, rating, types}
+    setSelectedPlace(placeInfo);
+  }
+
+  const handleItinerary = () => {
+    setShowPlace(false)
+    setShowItinerary(true);
+  }
+  const handlePlace = () => {
+    setPlace('');
+    setShowPlace(true)
+    setShowItinerary(false);
   }
 
   return (
     <>
-      <Sidebar />
+        <div className="sidebar">
+          <h1><span className="ai">AI</span> <span className="letter-t">T</span>ravels</h1>
+          <div className="gpt-buttons">
+            <button className='create-itinerary gpt-btn' onClick={handleItinerary}>Create Itinerary</button>
+            <button className='search-place gpt-btn' onClick={handlePlace}>Search Place</button>
+            <button className='gpt-btn'>Placeholder</button>
+          </div>
+        </div>
 
       <main>
-        <Place user_place={place}/>
+        {showPlace && <Place user_place={place} selectFunction={handleSelect}/>}
+        {showItinerary && <Itinerary place_info={selectedPlace}/>}
 
         <form action="#" onSubmit={handleSubmit} className='get-place-form'>
-          <input type="text" onChange={handleChange}/>
-          <button type='submit'>Search</button>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          <input type="text" onChange={handleChange} className='text-field' value={search}/>
+          <button type='submit' className='submit-button'>Search</button>
         </form>
 
       </main>
