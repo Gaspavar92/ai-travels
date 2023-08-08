94fe9212555e5b9017c6489e2a50cd9a0c62e3ee
Gaspare Tortora <g.tortora2@gmail.com>
2023-08-08 10:33:47 -0400

    Added additional props

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -4,50 +4,50 @@ import "./styles/Itinerary.css"
 
 import Loading from "./Loading";
 
-const Form = ({place_info}) => {
+const Itinerary = ({place_info}) => {
 
     console.log(place_info)
 
     const [response, setResponse] = useState('');
     const [loading, setLoading] = useState(false);
 
-    useEffect(() => {
-        const getResponse = async function() {
+    async function getResponse() {
+        
+        try {
+            setLoading(true);
+            setResponse('');
+            const apiKey = import.meta.env.VITE_API_KEY;
             
-            try {
-                    setLoading(true);
-                    setResponse('');
-                    const apiKey = import.meta.env.VITE_API_KEY;
+            const configuration = new Configuration({
+                apiKey: apiKey
+            });
+            const openai = new OpenAIApi(configuration);
+            const completion = await openai.createChatCompletion({
+                model: "gpt-3.5-turbo",
+                messages: [{"role": "system", "content": `You are a virtual travel agent; Provided a place to visit, you provide an itinerary with suggestions about restaurants, activities, places to see and things to do. You will also provide a summary with the place type. ALWAYS use HTML markup and use proper spacing between days. Don't use h1 and DO NOT provide additional styling.`}, {role: "user", content: `Place name: ${place_info.name}, place address: ${place_info.address}, place ratings: ${place_info.ratings}, place types: ${place_info.types.map(type => type)}, number of days: ${place_info.numberOfDays}`}],
+            });
             
-                    const configuration = new Configuration({
-                        apiKey: apiKey
-                    });
-                    const openai = new OpenAIApi(configuration);
-                    const completion = await openai.createChatCompletion({
-                    model: "gpt-3.5-turbo",
-                    messages: [{"role": "system", "content": `You are a virtual travel agent; Provided a place to visit, you provide an itinerary with suggestions about restaurants, activities, places to see and things to do.`}, {role: "user", content: `Place name: ${place_info.name}, place address: ${place_info.address}, place ratings: ${place_info.ratings}, place types: ${place_info.types.map(type => type)}`}],
-                    });
-            
-                    setResponse(completion.data.choices[0].message.content);
-                } catch {
-                   console.log('ERROR!')
-               } finally {
-                   setLoading(false)
-               }
-            }           
-            getResponse(); 
+            setResponse(completion.data.choices[0].message.content);
+        } catch {
+            console.log('ERROR!')
+        } finally {
+            setLoading(false)
+        }
+    }   
 
-    }, [place_info])
+    useEffect(() => {
+        getResponse();
+    }, [])
 
 
     return (
         loading ?
         <Loading /> :
         <div className="itinerary">
-            <p>{response}</p>
+        <p dangerouslySetInnerHTML={{ __html: response }}></p>
             <button className="save-itinerary">Save Itinerary</button>
         </div>
     )
 };
 
-export default Form;
+export default Itinerary;
