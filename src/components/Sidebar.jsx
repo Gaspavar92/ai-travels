ab4c0cfdd5329ab85351f7d22679cd74a478de51
Gaspare Tortora <g.tortora2@gmail.com>
2023-08-17 22:24:03 -0400

    Styled login information

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,13 +1,13 @@
 import "./styles/Sidebar.css";
 
-const Sidebar = ({handlePlace, handleLocations, handleSignUp, handleSignIn}) => {
+const Sidebar = ({handlePlace, handleLocations, handleSignIn}) => {
     return (
         <div className="sidebar">
         <h1><span className="ai">AI</span> <span className="letter-t">T</span>ravels</h1>
         <div className="gpt-buttons">
           <button className='search-place gpt-btn' onClick={handlePlace}><i className="fa-regular fa-map"></i>Instructions</button>
           <button className='gpt-btn' onClick={handleLocations}><i className="fa-solid fa-heart"></i>My Locations</button>
-          <button className='gpt-btn' onClick={handleSignUp}><i className="fa-solid fa-user"></i>Sign Up/In</button>
+          <button className='gpt-btn' onClick={handleSignIn}><i className="fa-solid fa-user"></i>Sign In</button>
         </div>
         <div className="logo">
           <img className='world' src="/logo.png" alt="3d world map" />
